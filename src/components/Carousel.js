27f5a58b2e3657412a5adbb905ee9c2be104fcb3
MrRhuezzler <anonymouspyro369@gmail.com>
2021-07-16 21:40:27 +0530

    Carousel Done

---
+++ src/components/Carousel.js
@@ -0,0 +1,71 @@
+import React, {useEffect, useRef, useState} from 'react';
+
+import '../styles/Carousel.css';
+
+import Image1 from '../assets/img1.jpg';
+import Image2 from '../assets/img2.jpg';
+
+const Carousel = () => {
+
+    const sliderData = [
+        {
+            image : Image1
+        },
+        {
+            image : Image2
+        }
+    ]
+
+    const length = sliderData.length;
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const nextSlide = () => {
+
+        let nextIndex = activeIndex + 1;
+
+        if(nextIndex >= length){
+            nextIndex = 0;
+        }
+
+        setActiveIndex(nextIndex);
+
+    }
+
+    useEffect(() => {
+
+        const intervalId = setInterval(nextSlide, 5000);
+
+        return () => {
+            clearInterval(intervalId);
+        }
+
+    });
+
+    return (
+        <div className="carousel-container">
+            <div className="carousel-slides">
+                {sliderData.map((child, index) => {
+                    return(
+                        <div key={index} className={`carousel-slide ${activeIndex === index ? 'carousel-slide-active' : ''}`}>
+                            <div className="carousel-images">
+
+                                {[...Array(4).fill()].map((_, index) => {
+                                    return (
+                                        <div key={index} className="carousel-image-part">
+                                            <div style={{backgroundImage: `url(${child.image})`}} className="carousel-image-part-inner"></div>
+                                        </div>
+                                    );
+                                })}
+
+                            </div>
+                        </div>
+                    );
+
+                })}
+            </div>
+        </div>
+    );
+
+}
+
+export default Carousel;
