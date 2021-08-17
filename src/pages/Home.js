344d29b4ac7b8d300799bb8517a7775e2ee78468
tgashwinkumar <tgashwinkumar@gmail.com>
2021-08-17 19:56:21 +0530

    mid commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,7 +25,6 @@ const Home = () => {
                         <div className="legacy-content">
                             &emsp;&emsp;&emsp;PSG Institute of Management was started as the Department of Management Sciences under the PSG College of Technology in the year 1964 to cater to the ever growing need of management graduates in this region which witnessed an industrial and entrepreneurial leap of faith that continues till today. The Department was granted the 'autonomous status' by the University of Madras and the University Grants Commission (UGC) in 1978 and metamorphosed into a full-fledged institute known as the PSG Institute of Management in 1994.
                         </div>   
-                        
                 </div>
                 <button className="legacy-knowmore font-head">
                     Know More
